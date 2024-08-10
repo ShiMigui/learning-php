97a844965010e092cbb34f0c50e8dff97a8307c7
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-08-10 19:15:16 -0300

    feat: Verifying if $value is String

--- verifying_type.php
+++ verifying_type.php
@@ -1,8 +1,24 @@
 <?php
 
-$value = 5;
-echo "Is int: ";
+$value = "Testing \$value";
+echo "value: $value";
+
+echo "<br>Is int: ";
 if (is_int($value)) {
+    echo "True";
+} else {
+    echo "False";
+}
+
+echo "<br>Is float: ";
+if (is_float($value)) {
+    echo "True";
+} else {
+    echo "False";
+}
+
+echo "<br>Is string: ";
+if (is_string($value)) {
     echo "True";
 } else {
     echo "False";

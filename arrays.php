a71459bff49e1b0a783517531ee5e02a86bc6014
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-08-11 20:48:14 -0300

    feat: Creating an associative array

--- arrays.php
+++ arrays.php
@@ -1,5 +1,13 @@
 <?php
 
 $a = [1, 2, 3];
-
 print_r($a);
+echo "<br>";
+
+$person = [
+    'name' => 'Miguel Nascimento dos Santos',
+    'birthdate' => '06/09/2005'
+];
+print_r($person);
+echo "<br>";
+echo $person['name'];

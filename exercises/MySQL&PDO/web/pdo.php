7197dfb3dc2b84f9c2081651070b97beca0b786e
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-12-08 20:40:34 -0300

    Basic queries: INSERT and SELECT

--- exercises/MySQL&PDO/web/pdo.php
+++ exercises/MySQL&PDO/web/pdo.php
@@ -7,6 +7,21 @@ $db = $_ENV['DB_NAME'];
 try {
     $con = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 
+    $stmt = $con->prepare("CALL insert_people(:nome, :email);");
+
+    $nome = "Lorem ipsum";
+    $email = "loremipsum@gmail.com";
+    $stmt->bindParam(':nome', $nome);
+    $stmt->bindParam(':email', $email);
+
+    $stmt->execute();
+
+    $result = $con->query("SELECT nm_people, nm_email FROM people");
+
+    $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($rows as $row) echo $row['nm_people']. " - ". $row['nm_email']. "<br>";
+
     echo "Connected successfully to the database!";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
